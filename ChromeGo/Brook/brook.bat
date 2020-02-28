bae24d9ff08d8aec402d1c30056ddfbb515fbfbb
bannedbook <banned.ebook@gmail.com>
2020-02-29 07:28:38 +0800

    update

--- ChromeGo/Brook/brook.bat
+++ ChromeGo/Brook/brook.bat
@@ -1,3 +1,3 @@
 CD /D "%~dp0"
-start brook client -l 127.0.0.1:2080 -i 127.0.0.1 -s 67.21.94.232:55555 -p ntdtv.com --http
+start brook client -l 127.0.0.1:2080 -i 127.0.0.1 -s 67.21.94.232:65535 -p dongtaiwang.com --http
 exit
--- ChromeGo/DAZE/ip_Update/ip_1.bat
+++ ChromeGo/DAZE/ip_Update/ip_1.bat
@@ -3,14 +3,14 @@ setlocal enableDelayedExpansion
 Title 从GitHub云端更新 DAZE 最新配置
 cd /d %~dp0
 
-set filename=config.ini
-..\..\wget -t 1 --ca-certificate=ca-bundle.crt  https://gitlab.com/free9999/ipupdate/-/raw/master/DAZE/config.ini
+set filename=daze.bat
+..\..\wget -t 3 --ca-certificate=ca-bundle.crt  https://killgcd.github.io/FirefoxFQ/FirefoxFQ/DAZE/daze.bat
 rem 检查文件是否存在，不存在则下载失败，存在则下载成功
 if exist %filename% goto startcopy
 
-set filename=config.ini
+set filename=daze.bat
 echo download ip1 failed,try download ip2 ...
-..\..\wget -t 1 --ca-certificate=ca-bundle.crt https://cdn.jsdelivr.net/gh/Alvin9999/PAC@latest/DAZE/config.ini
+..\..\wget -t 1 --ca-certificate=ca-bundle.crt https://raw.githubusercontent.com/killgcd/FirefoxFQ/master/FirefoxFQ/DAZE/daze.bat
 if exist %filename% goto startcopy
 
 rem 2次下载都失败，则提示用户反馈并退出
@@ -19,9 +19,9 @@ pause
 exit
 
 :startcopy
-del "..\config.ini_backup"
-ren "..\config.ini"  config.ini_backup
-copy /y "%~dp0%filename%" ..\config.ini
+del "..\daze.bat_backup"
+ren "..\daze.bat"  daze.bat_backup
+copy /y "%~dp0%filename%" ..\daze.bat
 del "%~dp0%filename%"
 ECHO.&ECHO.已更新完成最新可用DAZE配置,请按任意键退出,并重启程序. &PAUSE >NUL 2>NUL
 exit
