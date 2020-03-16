db8b09a4dd53d695167370b3dc232c29707242a1
bannedbook <banned.ebook@gmail.com>
2020-03-16 19:32:53 +0800

    update

--- ChromeGo/5.Lightsocks翻墙.cmd
+++ ChromeGo/5.Lightsocks翻墙.cmd
@@ -3,7 +3,7 @@ cls
 @echo off
 %1 start "" mshta vbscript:CreateObject("Shell.Application").ShellExecute("cmd.exe","/c %~s0 ::","","runas",1)(window.close)&&exit
 CD /D "%~dp0"
-echo 是否执行IP更新？IP更新从云端更新IP配置以解决封锁问题！
+echo 是否执行IP更新？IP更新从云端更新IP配置以解决封锁问题！第一次使用务必先更新IP.
 echo 按3跳过，按1选择ip1更新，若ip1不好用再按2选ip2更新，如果更新后都用不了，请发邮件到kebi2014@gmail.com进行反馈！
 choice /C 123 /T 15 /D 3 /M "1、ip1更新   2、ip2更新   3、跳过"
 if errorlevel 3 goto startfq
