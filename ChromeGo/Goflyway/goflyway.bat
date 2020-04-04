448ff330275d953ea4c3bcb3445722370bd85198
bannedbook <banned.ebook@gmail.com>
2020-04-04 20:34:11 +0800

    update

--- ChromeGo/Goflyway/goflyway.bat
+++ ChromeGo/Goflyway/goflyway.bat
@@ -1,4 +1,4 @@
 CD /D "%~dp0"
-goflyway -l="127.0.0.1:8100"  -up="cf://tr1.free99999.com:8880"  -k="dongtaiwang.com"
+goflyway -l="127.0.0.1:8100"  -up="cf://tr1.free99999.com:8080"  -k="dongtaiwang.com"
 pause
 exit
