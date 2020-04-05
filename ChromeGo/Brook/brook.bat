0a1328bad8f3da43040f1776cbce410f799e0145
bannedbook <banned.ebook@gmail.com>
2020-04-05 08:47:34 +0800

    update

--- ChromeGo/Brook/brook.bat
+++ ChromeGo/Brook/brook.bat
@@ -1,4 +1,4 @@
 CD /D "%~dp0"
-start brook wsclient -l 127.0.0.1:2080 -i 127.0.0.1 -s wss://www.free4444.xyz:443 -p dongtaiwang.com
+start brook wsclient -l 127.0.0.1:2080 -i 127.0.0.1 -s wss://tr2.free4444.xyz:443 -p dongtaiwang.com
 exit
 
