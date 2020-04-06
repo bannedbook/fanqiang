41720108fbad20e066477121fb310c447ae22ce1
bannedbook <banned.ebook@gmail.com>
2020-04-06 09:54:50 +0800

    update

--- ChromeGo/DAZE/daze.bat
+++ ChromeGo/DAZE/daze.bat
@@ -1,3 +1,3 @@
 CD /D "%~dp0"
-start daze_windows_386.exe client -s 216.244.76.220:12345 -k dongtaiwang.com -l 127.0.0.1:3080 -dns 1.1.1.1:53
+start daze_windows_386.exe client -s 45.144.3.201:33333 -k dongtaiwang.com -l 127.0.0.1:3080 -dns 1.1.1.1:53
 exit
