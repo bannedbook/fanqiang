1f718c90e4ad3e4dee56f7a169d2be49484b1a8f
bannedbook <banned.ebook@gmail.com>
2020-04-06 11:08:15 +0800

    update

---
+++ v2ss/server-cfg/v2ray-init.sh
@@ -0,0 +1,17 @@
+apt-get update
+iptables -A OUTPUT -p tcp --dport 25 -j REJECT
+apt-get install -y iptables-persistent
+wget https://raw.githubusercontent.com/bannedbook/blockporn/master/hosts.txt  -O ->> /etc/hosts
+echo '* soft nofile 51200' >> /etc/security/limits.conf
+echo '* hard nofile 51200' >> /etc/security/limits.conf
+ulimit -n 51200
+wget https://raw.githubusercontent.com/bannedbook/fanqiang/master/v2ss/server-cfg/sysctl.conf  -O -> /etc/sysctl.conf
+wget https://raw.githubusercontent.com/bannedbook/fanqiang/master/v2ss/server-cfg/resolv.conf  -O -> /etc/resolv.conf
+sysctl -p
+bash <(curl -L -s https://install.direct/go.sh)
+wget https://raw.githubusercontent.com/bannedbook/fanqiang/master/v2ss/server-cfg/v2ray.service  -O -> /etc/systemd/system/v2ray.service
+systemctl daemon-reload
+cp -f /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
+wget https://raw.githubusercontent.com/bannedbook/fanqiang/master/v2ss/server-cfg/v2ray_check.sh  -O -> /etc/v2ray/v2ray_check.sh
+wget https://raw.githubusercontent.com/bannedbook/fanqiang/master/v2ss/server-cfg/crontab.txt  -O -> /var/spool/cron/crontabs/root
+/etc/init.d/cron restart
