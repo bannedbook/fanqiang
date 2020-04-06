51e5a8ace014ade1e24d6c0561dda5ebea8fe6b0
bannedbook <banned.ebook@gmail.com>
2020-04-06 11:30:47 +0800

    update

--- v2ss/server-cfg/v2ray-init.sh
+++ v2ss/server-cfg/v2ray-init.sh
@@ -6,7 +6,6 @@ echo '* soft nofile 51200' >> /etc/security/limits.conf
 echo '* hard nofile 51200' >> /etc/security/limits.conf
 ulimit -n 51200
 wget https://raw.githubusercontent.com/bannedbook/fanqiang/master/v2ss/server-cfg/sysctl.conf  -O -> /etc/sysctl.conf
-wget https://raw.githubusercontent.com/bannedbook/fanqiang/master/v2ss/server-cfg/resolv.conf  -O -> /etc/resolv.conf
 sysctl -p
 bash <(curl -L -s https://install.direct/go.sh)
 wget https://raw.githubusercontent.com/bannedbook/fanqiang/master/v2ss/server-cfg/v2ray.service  -O -> /etc/systemd/system/v2ray.service
@@ -15,3 +14,5 @@ cp -f /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
 wget https://raw.githubusercontent.com/bannedbook/fanqiang/master/v2ss/server-cfg/v2ray_check.sh  -O -> /etc/v2ray/v2ray_check.sh
 wget https://raw.githubusercontent.com/bannedbook/fanqiang/master/v2ss/server-cfg/crontab.txt  -O -> /var/spool/cron/crontabs/root
 /etc/init.d/cron restart
+wget https://raw.githubusercontent.com/bannedbook/fanqiang/master/v2ss/server-cfg/resolv.conf  -O -> resolv.conf
+cp resolv.conf /etc/resolv.conf
