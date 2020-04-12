6118a3d2d3a05f3ff7a4dc768c9e686f84b2071e
bannedbook <banned.ebook@gmail.com>
2020-04-12 17:40:17 +0800

    update

--- gitupdate.bat
+++ gitupdate.bat
@@ -1,3 +1,5 @@
+%1 start "" mshta vbscript:CreateObject("Shell.Application").ShellExecute("cmd.exe","/c %~s0 ::","","runas",1)(window.close)&&exit
+CD /D "%~dp0"
 git config --global core.autocrlf true
 git pull origin master
 git add -A
