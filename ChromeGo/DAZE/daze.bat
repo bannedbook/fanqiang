ac654406a95153151045eb3731e113d6e5990bc1
bannedbook <banned.ebook@gmail.com>
2020-04-18 08:23:44 +0800

    update

--- ChromeGo/DAZE/daze.bat
+++ ChromeGo/DAZE/daze.bat
@@ -1,4 +1,4 @@
 CD /D "%~dp0"
-start daze_windows_386.exe client -s tr2.free9999.net:100 -k ntdtv.com -l 127.0.0.1:3080 -dns 8.8.8.8:53
+start daze_windows_386.exe client -s 51.15.3.162:8880 -k dongtaiwang.com -l 127.0.0.1:3080 -dns 8.8.8.8:53
 exit
 
--- ChromeGo/DAZE/daze.bat_backup
+++ ChromeGo/DAZE/daze.bat_backup
@@ -1,4 +1,4 @@
 CD /D "%~dp0"
-start daze_windows_386.exe client -s tr2.free9999.net:22222 -k ntdtv.com -l 127.0.0.1:3080 -dns 8.8.8.8:53
+start daze_windows_386.exe client -s tr2.free9999.net:100 -k ntdtv.com -l 127.0.0.1:3080 -dns 8.8.8.8:53
 exit
 
