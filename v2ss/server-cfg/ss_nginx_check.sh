131a6807b7cd5039b5a4cddc8317716217f254b7
bannedbook <banned.ebook@gmail.com>
2020-04-25 21:51:15 +0800

    update

---
+++ v2ss/server-cfg/ss_nginx_check.sh
@@ -0,0 +1,16 @@
+DATE=`date -d "today" +"%Y-%m-%d-%H:%M:%S"`
+MM=`ps -ef |grep /usr/local/bin/ss-server |grep -v grep |wc -l`
+if [ "$MM" == "0" ]; then 
+/usr/sbin/reboot
+echo "$DATE: The ss-server is down and restart" >> /var/log/ss_nginx_check.log 
+else 
+echo "$DATE: The ss-server is ok" >> /dev/null
+fi
+
+MM=`ps -ef |grep /usr/sbin/nginx |grep -v grep |wc -l`
+if [ "$MM" == "0" ]; then 
+/usr/sbin/reboot
+echo "$DATE: The nginx is down and restart" >> /var/log/ss_nginx_check.log 
+else 
+echo "$DATE: The nginx is ok" >> /dev/null
+fi
