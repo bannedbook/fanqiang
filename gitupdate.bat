9e10c87df61ec96d028f6b3c862a30f0dd9079c3
bannedbook <banned.ebook@gmail.com>
2020-05-01 11:25:05 +0800

    update

--- gitupdate.bat
+++ gitupdate.bat
@@ -1,8 +1,8 @@
-%1 start "" mshta vbscript:CreateObject("Shell.Application").ShellExecute("cmd.exe","/c %~s0 ::","","runas",1)(window.close)&&exit
-CD /D "%~dp0"
 git config --global core.autocrlf true
 git pull origin master
 git add -A
 git commit -m "update"
 git push origin master
+git tag -a "FQNews v5.0.6.8" -m "FQNews v5.0.6.8"
+git push origin --tags
 pause
