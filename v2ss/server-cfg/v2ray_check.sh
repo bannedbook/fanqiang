4b70af82cf6dfadf6ad06e173f4f4193c6c04779
bannedbook <banned.ebook@gmail.com>
2020-05-04 11:22:22 +0800

    update

--- v2ss/server-cfg/v2ray_check.sh
+++ v2ss/server-cfg/v2ray_check.sh
@@ -7,3 +7,12 @@ echo "$DATE: The v2ray is down and restart" >> /var/log/v2ray_check.log
 else 
 echo "$DATE: The v2ray is ok" >> /dev/null
 fi
+
+MM=`ps -ef |grep /usr/sbin/nginx |grep -v grep |wc -l`
+if [ "$MM" == "0" ]; then 
+/sbin/reboot
+/usr/sbin/reboot
+echo "$DATE: The nginx is down and restart" >> /var/log/ss_nginx_check.log 
+else 
+echo "$DATE: The nginx is ok" >> /dev/null
+fi
