5c277f128ffd7733040934eaaa83a3d1c6baeb22
bannedbook <banned.ebook@gmail.com>
2020-05-04 20:40:22 +0800

    update

--- v2ss/server-cfg/v2ray-init.sh
+++ v2ss/server-cfg/v2ray-init.sh
@@ -12,8 +12,6 @@ bash <(curl -L -s https://install.direct/go.sh)
 wget https://raw.githubusercontent.com/bannedbook/fanqiang/master/v2ss/server-cfg/v2ray.service  -O -> /etc/systemd/system/v2ray.service
 systemctl daemon-reload
 cp -f /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
-wget https://raw.githubusercontent.com/bannedbook/fanqiang/master/v2ss/server-cfg/crontab.txt  -O -> /var/spool/cron/crontabs/root
-/etc/init.d/cron restart
 wget https://raw.githubusercontent.com/bannedbook/fanqiang/master/v2ss/server-cfg/resolv.conf  -O -> resolv.conf
 cp resolv.conf /etc/resolv.conf
 service v2ray start
@@ -23,3 +21,7 @@ nginx -t
 nginx -s reload
 wget https://raw.githubusercontent.com/bannedbook/fanqiang/master/v2ss/server-cfg/v2ray_check.sh  -O -> /etc/v2ray/v2ray_check.sh
 chmod +x /etc/v2ray/v2ray_check.sh
+cat /etc/v2ray/v2ray_check.sh
+wget https://raw.githubusercontent.com/bannedbook/fanqiang/master/v2ss/server-cfg/crontab.txt  -O -> /var/spool/cron/crontabs/root
+/etc/init.d/cron restart
+
