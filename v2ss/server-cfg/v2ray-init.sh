baaed9bb35dbd800763afe3a9dea222f243368ae
bannedbook <banned.ebook@gmail.com>
2020-08-16 13:18:30 +0800

    update

--- v2ss/server-cfg/v2ray-init.sh
+++ v2ss/server-cfg/v2ray-init.sh
@@ -1,29 +1,40 @@
 #!/usr/bin/env bash
 set -x
+#trap read debug
 apt-get update
 apt-get install curl wget -y
+bash <(curl -L -s https://github.com/v2fly/fhs-install-v2ray)
 iptables -A OUTPUT -p tcp --dport 25 -j REJECT
-apt-get install -y iptables-persistent
+hostbakfile=/etc/hosts_bak
+if [ -f "$hostbakfile" ]; then
+    cp -f /etc/hosts_bak /etc/hosts
+else 
+    cp -f /etc/hosts /etc/hosts_bak
+fi
 wget https://raw.githubusercontent.com/bannedbook/blockporn/master/hosts.txt  -O ->> /etc/hosts
 echo '* soft nofile 51200' >> /etc/security/limits.conf
 echo '* hard nofile 51200' >> /etc/security/limits.conf
 ulimit -n 51200
 wget https://raw.githubusercontent.com/bannedbook/fanqiang/master/v2ss/server-cfg/sysctl.conf  -O -> /etc/sysctl.conf
 sysctl -p
-bash <(curl -L -s https://install.direct/go.sh)
 wget https://raw.githubusercontent.com/bannedbook/fanqiang/master/v2ss/server-cfg/v2ray.service  -O -> /etc/systemd/system/v2ray.service
 systemctl daemon-reload
 timedatectl set-timezone Asia/Singapore
 wget https://raw.githubusercontent.com/bannedbook/fanqiang/master/v2ss/server-cfg/resolv.conf  -O -> resolv.conf
 cp resolv.conf /etc/resolv.conf
-service v2ray start
+#below need step to step
+apt-get install -y iptables-persistent
 apt-get -y install nginx
+#above need step to step
 wget https://raw.githubusercontent.com/bannedbook/fanqiang/master/v2ss/server-cfg/v2ss-nginx.conf  -O -> /etc/nginx/nginx.conf
 nginx -t
 nginx -s reload
+/usr/sbin/service v2ray restart
+/sbin/service v2ray restart
 wget https://raw.githubusercontent.com/bannedbook/fanqiang/master/v2ss/server-cfg/v2ray_check.sh  -O -> /etc/v2ray/v2ray_check.sh
 chmod +x /etc/v2ray/v2ray_check.sh
 cat /etc/v2ray/v2ray_check.sh
 wget https://raw.githubusercontent.com/bannedbook/fanqiang/master/v2ss/server-cfg/crontab.txt  -O -> /var/spool/cron/crontabs/root
-/etc/init.d/cron restart
+#crontab -e to active cron
+
 
