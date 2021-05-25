baee928d340cbe92a078644866cbda710e7b3c23
bannedbook <banned.ebook@gmail.com>
2021-05-25 21:03:22 +0800

    update

--- gitupdate.bat
+++ gitupdate.bat
@@ -4,6 +4,6 @@ git add -A
 git commit -m "update"
 git push origin master
 rem git tag -a "FQNews-v1.2.1" -m "FQNews-v1.2.1"
-git tag -a "ChromeGo-v20210420" -m "ChromeGo-v20210420"
+git tag -a "ChromeGo-v20210525" -m "ChromeGo-v20210525"
 git push origin --tags
 pause
